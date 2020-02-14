8f51aba4671cf51eef7ca08c6f91b9ad4864d86b
amurashko24 <amurashko24@gmail.com>
2020-02-14 15:48:33 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setuptools
+
+setup(
+	name="mycalc",
+	version="0.0.1",
+	description="A small example package with some arithmetics",
+	packages=find_packages(),
+	include_package_data=True,
+	author="Amazing Author",
+	author_email="author@example.com"
+	url="https://github.com/murateam/m21_test_calc/"
+)
